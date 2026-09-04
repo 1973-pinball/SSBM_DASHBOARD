d1f9cccc203010f9bb45fd1396d03e920dee36ec
Studio Pinball <info.studio.pinball@gmail.com>
2026-09-03 21:20:20 -0400

    Update footer GitHub repository link

--- src/App.tsx
+++ src/App.tsx
@@ -1335,7 +1335,7 @@ export default function App() {
         <a href="/about">About</a>
         <a href="/metrics">Metrics</a>
         <a href="/melee-majors">Melee majors</a>
-        <a href="https://github.com/1973-pinball/SSBM_DASHBOARD">Source</a>
+        <a href="https://github.com/1973-pinball/SSBM_STATS">GitHub Repo</a>
         <a href="mailto:info.studio.pinball@gmail.com">info.studio.pinball@gmail.com</a>
         <button className="footer-link" onClick={() => openOverlay("privacy")}>Privacy promise</button>
       </footer>
